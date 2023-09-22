1ac808016ed017b2aefefcf803cfddcbc1436f91
Dominique Ware <dominiqueware34@yahoo.com>
2023-09-21 19:40:12 -0500

    feat: created chat layout and reorganized folder layout

---
+++ app/(messenger)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<main className="min-h-screen grid grid-cols-12">
+			<aside className="col-span-3 p-4 border-r-2">contacts</aside>
+			<section className="col-span-9">chat</section>
+		</main>
+	)
+}
